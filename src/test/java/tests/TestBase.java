7813434f8f60a20a1d089519f2f5bf814e34bd4a
AlexLatalin <a.latalin@myrambler.ru>
2025-04-11 16:02:05 +0300

    update TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -30,7 +30,7 @@ public class TestBase {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@" + System.getProperty("remoteHost") + "/wd/hub";
+        Configuration.remote = System.getProperty("remoteUrl");
 
         RestAssured.baseURI = BASE_URL;
 
